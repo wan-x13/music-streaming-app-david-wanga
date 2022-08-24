90e6b9c271706a63ed6c36a3ac3e9986ea53b753
David Wanga <davidwangajean@gmail.com>
2022-08-24 16:56:07 +0100

    merge 'this"

--- src/components/TopPlaylist/toplaylistStyle.js
+++ src/components/TopPlaylist/toplaylistStyle.js
@@ -6,8 +6,7 @@ export const TopPlaylistContainer = styled.div`
         margin-top : 20px;
         margin-bottom : 50px;
       h3{
-        font-size : 2vw;
-        padding-left: 2vw;
+        font-size : 30px;
       }
        
         
@@ -17,27 +16,23 @@ export const CardContainer = styled.div`
         margin : 60px 30px 30px 30px;
         display: grid;
         grid-template-columns: repeat(4, 1fr);
-<<<<<<< HEAD
         gap : 2vw;
-=======
-        /* grid-gap : 0.1vw; */
->>>>>>> develop
 
        
 
        `
 export const CardContent = styled.div`
-            width : 16vw;
+            width : 230px;
             background : #232327;
             display : flex; 
             flex-direction : column;
             justify-content : center;
             align-items: center;
-            border-radius : 1.5vw;
+            border-radius : 20Px;
             margin-top: -40px;
 
             img{
-            width : 14vw;
+            width : 200px;
             border-radius: 10px;
             box-shadow: 0px 4px 4px 0px #00000040;
             margin-top : 20px;
