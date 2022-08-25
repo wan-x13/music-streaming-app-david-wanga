5e69e9860a62e5cd7218a0f93e65e5b140b65ab8
David Wanga <davidwangajean@gmail.com>
2022-08-25 09:41:33 +0100

    fix userToken in player component

--- src/components/PLayer/PLayer.jsx
+++ src/components/PLayer/PLayer.jsx
@@ -22,7 +22,7 @@ const PLayer = () => {
     return (
         <SpotifyPlayer
 
-          token={'BQCEqAEXZDA2R6X9mGXaMVwnbnRNGlELtQ3tIhNmp_ZEOuP32iCUe1ln9F3As3trNIFy6GJCCY-CAmbAleCpKpYU4hs_ZFnmHkOdhe11Op5H-t535Fe3Nk3-gEr9L_hAiI5W_yJ7z0B5Y8_ZlgMt1F1ct6UXfzoWgnnsfWrzylO_l3kxJHlaIlmMJ8KRhz-K2OqbyDWRXs0cmf4IlRUzugrkwmc1N_scqEyYTwfNOgAR-XFvIJ-1PlDnDAaYEXJllbeKbuqDMKQPriEw_0gs0_eVHi51pzlDkz-VjmfUgWqzxQxrYQ_k4EYYPOp_d-tdqI8BcCmxuGWJIsRWy00'}
+          token={userToken}
           uris = {[uri]}
           initialVolume = {initialVolume}
           play={play}
